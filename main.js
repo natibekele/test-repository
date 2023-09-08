da9581abe926dd9a0327c05abca59f06799903b8
nati <natibekele@gmail.com>
2023-09-08 18:03:17 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hi there 👋!</h1>
+
+    <h3> I just wanted to test out deploying a subdomain </h3>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
